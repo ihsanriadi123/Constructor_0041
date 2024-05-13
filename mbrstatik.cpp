2708953bb4ab8b2a7893e62127ee0122b38b7928
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-05-13 15:02:21 +0700

    membuat class mahasiswa dapaet di setting supaya aman

--- mbrstatik.cpp
+++ mbrstatik.cpp
@@ -15,3 +15,29 @@ class mahasiswa {
         }
 };
 
+int mahasiswa::nim = 20;
+
+void mahasiswa::setid() {
+    id = nim++;
+}
+
+void mahasiswa::printall() {
+    cout << "id = " << id << endl;
+    cout << "nama = " << nama << endl;
+    cout << endl;
+}
+
+int main() {
+    mahasiswa mhs1("Lia Kurnia");
+    mahasiswa mhs2("Asroni");
+    mahasiswa mhs3("Andi Kurniawan");
+    mahasiswa mhs4("Joko Purbo");
+
+
+    mhs1.printall();
+
+    mhs2.printall();
+    mhs3.printall();
+    mhs4.printall();
+    return 0;
+}
---
+++ mbrstatikfunc.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+    private:
+        static int nim;
+
+    public:
+        int id;
+        string nama;
+        
+
+}
